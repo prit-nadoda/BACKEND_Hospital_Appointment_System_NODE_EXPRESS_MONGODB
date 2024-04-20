9e6cc795a610b7d286a1eb4b7d6780de62af54cc
prit-nadoda <prit.nadoda.123@gmail.com>
2024-04-20 22:34:04 +0530

    Patient Login with JWT auth token!

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,9 @@
 import { catchAsyncError } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { User } from "../models/userSchema.js";
+import { generateToken } from "../utils/jwkToken.js";
 
-export const petientRegister = catchAsyncError(async (req, res, next) => {
+export const patientRegister = catchAsyncError(async (req, res, next) => {
   const {
     firstname,
     lastname,
@@ -28,8 +29,8 @@ export const petientRegister = catchAsyncError(async (req, res, next) => {
     return next(new ErrorHandler("Please fill all the fields!", 400));
   }
 
-  let user = await User.findOne({ email });
-  if (user) {
+  let isUserExist = await User.findOne({ email });
+  if (isUserExist) {
     return next(
       new ErrorHandler(
         "User already Registrated with given Email Address!",
@@ -38,7 +39,7 @@ export const petientRegister = catchAsyncError(async (req, res, next) => {
     );
   }
 
-  const Petient = await User.create({
+  const user = await User.create({
     firstname: firstname,
     lastname: lastname,
     email: email,
@@ -49,10 +50,27 @@ export const petientRegister = catchAsyncError(async (req, res, next) => {
     type: type,
     // doctorDepartment: doctorDepartment,
     // docAvatar: docAvatar,
-  }).then(() => {
-    res.status(200).json({
-      success: true,
-      message: "Petient registered successfully!",
-    });
   });
+  generateToken(user, "patient registered successfully!", 200, res);
+});
+
+export const login = catchAsyncError(async (req, res, next) => {
+  const { email, password, conformPassword } = req.body;
+  if (!email || !password || !conformPassword) {
+    return next(new ErrorHandler("Please provide full information!!", 400));
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 400));
+  }
+  const isPasswordMatched = await user.conformPassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Passwords do not macth!", 400));
+  }
+  if (role !== user.role) {
+    return next(
+      new ErrorHandler("You are not authorized to access this resource", 400)
+    );
+  }
+  generateToken(user, "You are logged in successfully!", 200, res);
 });
--- models/userSchema.js
+++ models/userSchema.js
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema({
   },
   type: {
     type: String,
-    enum: ["Petient", "Admin", "Doctor"],
+    enum: ["Patient", "Admin", "Doctor"],
     default: "User",
   },
   doctorDepartment: {
